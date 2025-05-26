2428f82213b59f15cdec87313ac040800c33e415
Aleksandr Safiev <aleksandr.safiev@yandex.ru>
2025-05-26 20:23:28 +0300

    pm2 config

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
             port: '3000',
             exec_mode: 'cluster',
             instances: 'max',
-            script: './.output/server/index.mjs',
+            script: 'frontend/.output/server/index.mjs',
         },
     ],
 };
