fefdb6be580df72b52da9f5cb3bbb92319bb964e
Aleksandr Safiev <aleksandr.safiev@yandex.ru>
2025-08-07 19:49:44 +0300

    fix fonts

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -6,6 +6,9 @@ module.exports = {
             exec_mode: 'cluster',
             instances: 'max',
             script: 'frontend/.output/server/index.mjs',
+            env: {
+                NUXT_PUBLIC_YANDEX_METRIKA_ID: '103571975'
+            }
         },
     ],
 };
